a30ef9c2bf523ed9c02202fe8316e0b2b4d36f0c
pixyda-sannohe <sannohe@pixyda.co.jp>
2021-08-24 19:28:11 +0900

    プロジェクトデータ再アップ

---
+++ dango_test01/Assets/Scripts/main_ctr.cs
@@ -0,0 +1,266 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main_ctr : MonoBehaviour
+{
+    //タッチエフェクト用
+    public GameObject prefab;
+
+    //外部スクリプトアクセス用
+    private Score  Score;
+    private timer  Timer;
+
+    //カメラ位置調整用
+    private GameObject cam_obj;
+
+    //ダンゴムシプレハブ
+    public GameObject dango;
+
+    //フェードエフェクト
+    public GameObject ui_fade;
+
+    //public int dango_cnt;
+
+    //クリアーフラグ
+    [HideInInspector]
+    public bool clear_st;
+
+    //ゲームオーバーフラグ
+    [HideInInspector]
+    public bool gameover_st;
+
+　　//カメラ位置調整用
+    private bool camset;
+    private Vector3 cam_pos1;
+    private Vector3 cam_pos2;
+
+    //switchのstate
+    private int state;
+
+    //現在のステージナンバー
+    public int stage_num;
+
+    //ステージ別ダンゴムシの数リスト
+    public List<int> DangoCntList = new List<int>();
+
+    //ステージ別ダンゴムシのノルマリスト
+    public List<int> DangoNormList = new List<int>();
+
+    //ステージプレファブリスト
+    public List<GameObject> StageList = new List<GameObject>();
+
+    // ステージの親にするオブジェクト
+    public GameObject Stage;
+
+    // ステージナンバー表示
+    public Text StageNumText;
+
+    //ステージサイズ確認用
+    private int stage_size_st;
+    
+    //天敵のON/OFF
+    private bool enemy_st;
+
+    //天敵プレファブリスト
+    public List<GameObject> EnemyList = new List<GameObject>();
+
+    //public float deleteTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state=0;
+        stage_num=1;
+        enemy_st=false;
+        camset=false;
+        clear_st=false;
+        gameover_st=false;
+
+        //スコアスクリプトアクセス用
+        Score=GameObject.Find("Canvas/ScorePanel").gameObject.GetComponent<Score>();
+
+        //タイマースクリプトアクセス用
+        Timer=GameObject.Find("Canvas/timer").gameObject.GetComponent<timer>();
+
+        //cameraオブジェクトを検索
+        cam_obj=GameObject.Find("camera_pos").gameObject;
+
+        //Debug.Log("ステージ数="+StageList.Count);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //タッチエフェクト
+        if(Input.GetMouseButtonDown(0))
+        {
+            //マウスカーソルの位置を取得。
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = 5f;
+             GameObject clone = Instantiate(prefab, Camera.main.ScreenToWorldPoint(mousePosition),
+                 Quaternion.identity);
+            //Destroy(clone, deleteTime);
+
+        }
+
+        switch (state)
+        {
+            case 0:
+                //カメラの初期位置
+                cam_obj.transform.position=new Vector3( 0f, 10f, -3f );
+
+                //ステージ読み込み
+                GameObject StageObject = Object.Instantiate(StageList[stage_num-1]) as GameObject;
+                StageObject.transform.SetParent(Stage.transform, false);
+
+                StageNumText.text = "STAGE 0"+stage_num.ToString();
+
+
+                //ステージサイズによるカメラの固定位置
+                if(GameObject.Find ("size_s")){
+                    stage_size_st=0;
+                    cam_pos2=new Vector3( 0f, 34f, -5f );
+                }else if(GameObject.Find ("size_m")){
+                    stage_size_st=1;
+                    cam_pos2=new Vector3( 0f, 60f, -10f );
+                }
+                //ダンゴムシ読み込み
+                for(int i=0; i < DangoCntList[stage_num-1]; i++){
+                    Invoke("dango_born", i*0.1f);
+                }
+
+                state=5;
+
+                break;
+            case 5:
+                ui_fade.GetComponent<Animator>().Play("fade_in");
+
+                if(!camset){
+                    cam_obj.transform.position=new Vector3( 0,cam_obj.transform.position.y+(cam_pos2.y-cam_obj.transform.position.y)*0.015f, cam_obj.transform.position.z+(cam_pos2.z-cam_obj.transform.position.z)*0.015f );
+                    cam_pos1=cam_obj.transform.position;
+
+                    if(cam_pos2.y-cam_obj.transform.position.y<0.1f){
+                        cam_obj.transform.position=cam_pos2;
+                        
+                        state=10;
+                    }
+                }
+                
+                break;
+            case 10:
+                //Debug.Log("カメラストップ！");
+                
+                state=11;
+                break;
+            case 11:
+                //ダンゴムシ読み込み
+                /*for(int i=0; i < dango_cnt; i++){
+                    Invoke("dango_born", i*0.1f);
+                }*/
+                
+                Timer.timerActive=true;
+
+                state=15;
+                
+                break;
+            case 15:
+                //天敵読み込み
+                if(enemy_st){
+                //if(GameObject.Find ("size_s")){    
+                    GameObject EnemyObject = Object.Instantiate(EnemyList[0]) as GameObject;
+                    EnemyObject.transform.Translate(0, 1, 0);
+                }
+                
+                state=30;
+                break;
+                
+            case 30:    
+                        
+                if(clear_st){
+                    //Time.timeScale = 10f;
+                    ui_fade.GetComponent<Animator>().Play("fade_out");
+                    Timer.timerActive=false;
+                    if(stage_num==StageList.Count){
+                        stage_num=1;
+                    }else{
+                        stage_num++;    
+                    }
+                    
+                    state=50;
+                }
+
+                if(gameover_st){
+                    //Time.timeScale = 10f;
+                    ui_fade.GetComponent<Animator>().Play("fade_out");
+                    Timer.timerActive=false;
+                    state=50;
+                }
+
+                break;
+            case 50:
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                   //リセット
+                   GameObject stage = GameObject.Find("stage").gameObject;
+                   GameObject stage_child = stage.transform.GetChild(0).gameObject;
+                   Destroy(stage_child);
+                   ui_fade.GetComponent<Animator>().Play("fade_def");
+                   clear_st=false;
+                   gameover_st=false;
+                   Score.ScoreReset(stage_num);
+                   Timer.seconds = 0f;
+                   Timer.timerText.text =Timer.seconds.ToString("F1")+"sec";
+                   state=0;
+
+                }
+                break;
+            default:
+                
+                break;
+        }
+
+
+    }
+
+    //ダンゴムシ生成メソッド
+    void dango_born(){
+        GameObject copied = Object.Instantiate(dango) as GameObject;
+
+        if(stage_size_st==0){
+            
+            copied.transform.Translate(Random.Range(-4,4), 7, Random.Range(-4,4));
+            int f=Random.Range(0,4);
+            if(f==0){
+                copied.transform.Rotate(0, 90, 0);
+                
+            }else if(f==1){
+                copied.transform.Rotate(0, -90, 0);
+                
+            }else if(f==2){
+                copied.transform.Rotate(0, 180, 0);
+                
+            }
+        }else if(stage_size_st==1){
+            
+            copied.transform.Translate(Random.Range(-9,9), 7, Random.Range(-9,9));
+            int f=Random.Range(0,4);
+            if(f==0){
+                copied.transform.Rotate(0, 90, 0);
+                
+            }else if(f==1){
+                copied.transform.Rotate(0, -90, 0);
+                
+            }else if(f==2){
+                copied.transform.Rotate(0, 180, 0);
+                
+            }
+        }
+
+    }
+
+    
+}
---
+++ dango_test01/Assets/Scripts/wall.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    private BoxCollider col;
+    private GameObject wall_def;
+    private GameObject wall_pos;
+    private GameObject wall_break;
+    private Material material;
+    private Color defColor;
+
+    //private float hit_cnt;
+
+    private bool up_state;
+    public bool col_state;
+
+    //private bool but_down;
+    Vector3 screenPoint;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hit_cnt=0;
+        up_state=false;
+        col_state=false;
+        //but_down=false;
+        wall_def=transform.Find("wall_obj").gameObject;
+        wall_pos=transform.Find("wall_pos").gameObject;
+        wall_break=transform.Find("break").gameObject;
+        wall_break.SetActive(false);
+        col= wall_def.GetComponent<BoxCollider>();
+        material = wall_def.GetComponent<Renderer>().material;
+        defColor = material.color;
+        //Debug.Log(defColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //持ち上げている間はマウスの位置に追従
+        if(up_state){
+            this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 a = new Vector3 (Input.mousePosition.x,Input.mousePosition.y,screenPoint.z);
+            //Vector3 a = new Vector3 (0,0,0);
+         
+
+            float ax=Camera.main.ScreenToWorldPoint (a).x;
+            float ay=this.transform.position.y;
+            float az=Camera.main.ScreenToWorldPoint (a).z;
+            //transform.position = Camera.main.ScreenToWorldPoint (a);
+            
+            
+            transform.position =  new Vector3 (ax,ay,az);
+
+            
+            if (Input.GetMouseButtonDown(0) && !col_state) {
+                transform.position =  new Vector3 (transform.position.x,this.transform.position.y,transform.position.z);
+                wall_def.transform.localPosition =new Vector3(0, 1, 0);
+                defColor = material.color;
+                material.color=new Color(defColor.r,defColor.g,defColor.b,1f);
+                up_state=false;
+
+                //but_down=true;
+                //Debug.Log("置いた1:"+but_down+" up_state:"+up_state);
+                
+            }
+
+            
+        }else{
+            if (Input.GetMouseButtonDown(0)) {
+    
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit = new RaycastHit();
+        
+                if (Physics.Raycast(ray, out hit)){
+                    //Rayがcolにヒットした場合
+                    if (hit.collider == col){
+                        wall_def.transform.localPosition =new Vector3(0, 10, 0);
+                    
+                        material.color=new Color(defColor.r,defColor.g,defColor.b,0.8f);
+                        up_state=true;
+                        //Debug.Log("hit!");
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+
+    //ダメージを受けた場合
+    public void change_col() {
+        if(defColor.g>0){
+            defColor.g-=0.2f;
+        }else{
+            wall_def.SetActive(false);
+            wall_pos.SetActive(false);
+            wall_break.SetActive(true);
+        }
+        material.color=new Color(defColor.r,defColor.g,defColor.b,1f);
+        //material.color=new Color(1f,0f,0f,1f);
+        defColor = material.color;
+        
+    }
+
+    //置けない場合
+    public void not_down() {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit)){
+            //Rayがcolにヒットした場合
+            if (hit.collider == col){
+                wall_def.transform.localPosition =new Vector3(0, 10, 0);
+            
+                material.color=new Color(defColor.r,defColor.g,defColor.b,0.8f);
+                up_state=true;
+                //Debug.Log("hit!");
+            }
+            
+        }
+        
+    }
+
+}
