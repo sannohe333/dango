a42ed496a553e8626a7f93db0a690c9dcd08deb9
pix_kato <kato@pixyda.co.jp>
2021-10-20 17:20:59 +0900

    json保持に使うUserDataInfo.csのクラス構造を変更

--- dango_test01/Assets/Scripts/Data/UserDataInfo.cs
+++ dango_test01/Assets/Scripts/Data/UserDataInfo.cs
@@ -5,43 +5,45 @@ using System.IO;
 
 public class UserDataInfo
 {
-	/// <summary>
-	/// クリアしたステージ数
-	/// </summary>
-	public int clearStage;
+	public class User{
+		/// <summary>
+		/// クリアしたステージ数
+		/// </summary>
+		public int clearStage;
 
-	/// <summary>
-	/// コレクトしたダンゴキャラのID配列。
-	/// ※このIDはDangoInfoクラスで設定されているDangoInfo.Dango.idの値を見ている
-	/// </summary>
-	public List<int> collectDangoIdList;
+		/// <summary>
+		/// コレクトしたダンゴキャラのID配列。
+		/// ※このIDはDangoInfoクラスで設定されているDangoInfo.Dango.idの値を見ている
+		/// </summary>
+		public List<int> collectDangoIdList;
+	}
 
-
-	/// <summary>
-	/// userData.jsonを取得する
-	/// </summary>
-	/// <returns>userData.jsonを代入したUserDataInfoを返す</returns>
-	public UserDataInfo LoadUserData(){
+/// <summary>
+/// userData.jsonを取得する
+/// </summary>
+/// <returns>userData.jsonを代入したUserDataInfoを返す</returns>
+public UserDataInfo.User LoadUserData(){
 		//初めに保存先を計算する　Application.dataPathで今開いているUnityプロジェクトのAssetsフォルダ直下を指定して、後ろに保存名を書く
 		var dataPath = Application.dataPath + "/Resources/Json/userData.json";
 		StreamReader reader = new StreamReader(dataPath); //受け取ったパスのファイルを読み込む
 		string dataStr = reader.ReadToEnd();//ファイルの中身をすべて読み込む
 		reader.Close();//ファイルを閉じる
 
-		return JsonUtility.FromJson< UserDataInfo > (dataStr);//読み込んだJSONファイルをPlayerData型に変換して返す
+		return JsonUtility.FromJson<UserDataInfo.User> (dataStr);//読み込んだJSONファイルをPlayerData型に変換して返す
 	}
 
 	/// <summary>
 	/// ユーザーデータをuserData.jsonに保存する
 	/// </summary>
 	/// <param name="user">保存するuserData</param>	
-	public void SaveUserData(UserDataInfo user)
+	public void SaveUserData(UserDataInfo.User user)
 	{
 		//初めに保存先を計算する　Application.dataPathで今開いているUnityプロジェクトのAssetsフォルダ直下を指定して、後ろに保存名を書く
 		string dataPath = Application.dataPath + "/Resources/Json/userData.json";
 
 		// userのプレイデータをjsonテキストに変換
-		UserDataInfo _user = new UserDataInfo();
+		UserDataInfo.User _user = new UserDataInfo.User();
+
 		_user.clearStage = user.clearStage;
 		_user.collectDangoIdList = user.collectDangoIdList;
 
