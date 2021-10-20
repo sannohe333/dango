162878aa57576cf7bbd8a8bcf7727d422865496e
pix_kato <kato@pixyda.co.jp>
2021-10-20 17:18:32 +0900

    使ってないコードの削除とインデントをタブに変更

--- dango_test01/Assets/Scripts/Title.cs
+++ dango_test01/Assets/Scripts/Title.cs
@@ -1,36 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Linq;
-using System;
 
 public class Title : MonoBehaviour
 {
+	UserDataInfo userDataInfo = new UserDataInfo();
 
-    void Awake()
-    {
-         /** 既にシーンが読み込まれているかどうか */
+	void Awake()
+	{
+		/** 既にシーンが読み込まれているかどうか */
+		if (!SceneController.AlreadyLoadScene("Common"))
+		{
+		SceneManager.LoadScene("Common", LoadSceneMode.Additive);
+		}
+	}
 
-        if (!SceneController.AlreadyLoadScene("Common"))
-        {
-           SceneManager.LoadScene("Common", LoadSceneMode.Additive);
-        }
-    }
+	public void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*if(Input.GetMouseButtonDown(0))
-        {
-            //既にシーンが読み込まれているかどうか
-           
-            if (SceneController.AlreadyLoadScene("Title"))
-            {
-                SceneManager.UnloadSceneAsync("Title");
-            }
-            
-            SceneManager.LoadScene("StageSelect", LoadSceneMode.Additive);
-        }*/
-    }
+	}
 }
